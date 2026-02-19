987f3deb73859b1a8bef5bbbb9c02c1ecdb92778
A-Letourneau <aleletourneau123@gmail.com>
2026-02-19 17:17:49 -0500

    accel fonctionnel

--- code unitaire/Arduino ACCELEROMETER/src/main.cpp
+++ code unitaire/Arduino ACCELEROMETER/src/main.cpp
@@ -6,24 +6,28 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-#define Z_AXIS_PIN A0
 #define REFRESH_RATE 100
 #define X_AXIS_PIN A2
 #define Y_AXIS_PIN A3
 #define Z_AXIS_PIN A4
 
 int ReadAxis(int axisPin);
+void getMove();
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 3, en = 4 , d4 = 8, d5 = 9, d6 = 10, d7 = 11;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 unsigned long startTime = 0;
-int RawMinRest = 260;
-int RawMaxRest = 410;
+int RawMinRest = 255;
+int RawMaxRest = 405;
 long somme = 0;
 long cptSomme = 0;
 const int sampleSize = 10;
+
+int xRestCpt = 0;
+int yRestCpt = 0;
+int zRestCpt = 0;
 
 void setup() {
 
@@ -35,7 +39,7 @@ void setup() {
 
 void loop() {
 
-  //Read raw values
+  /*//Read raw values
   int xRaw = ReadAxis(X_AXIS_PIN);
   int yRaw = ReadAxis(Y_AXIS_PIN);
   int zRaw = ReadAxis(Z_AXIS_PIN);
@@ -50,19 +54,19 @@ void loop() {
   float yAccel = yScaled / 1000.0;
   float zAccel = zScaled / 1000.0;
 
-  Serial.print("X, Y, Z  :: ");
-  Serial.print(xRaw);
-  Serial.print(", ");
-  Serial.print(yRaw);
-  Serial.print(", ");
-  Serial.print(zRaw);
-  Serial.print(" :: ");
-  Serial.print(xAccel );
-  Serial.print("G, ");
-  Serial.print(yAccel );
-  Serial.print("G, ");
-  Serial.print(zAccel);
-  Serial.println("G");
+
+  lcd.setCursor(0,0);
+  lcd.print("X ");
+  lcd.print(xAccel, 1);
+  lcd.setCursor(9,0);
+  lcd.print("Y ");
+  lcd.print(yAccel, 1);
+  lcd.setCursor(0,1);
+  lcd.print("Z ");
+  lcd.print(zAccel, 1);*/
+
+  getMove();
+
 }
 
 // Take samples and return the average
@@ -74,4 +78,125 @@ int ReadAxis(int axisPin) {
     reading += analogRead(axisPin);
   }
   return reading / sampleSize;
+}
+
+/*void getMove()
+{
+  // Convert raw values to 'milli-Gs"
+  long firstXScaled = map(ReadAxis(X_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+  long firstYScaled = map(ReadAxis(Y_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+  long firstZScaled = map(ReadAxis(Z_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+
+  delay(50);
+
+  // Convert raw values to 'milli-Gs"
+  long secondXScaled = map(ReadAxis(X_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+  long secondYScaled = map(ReadAxis(Y_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+  long secondZScaled = map(ReadAxis(Z_AXIS_PIN), RawMinRest, RawMaxRest, -1000, 1000);
+
+  long differenceX = firstXScaled - secondXScaled;
+  lcd.setCursor(0,1);
+  lcd.print("        ");
+  lcd.setCursor(0,1);
+  lcd.print(differenceX);
+  lcd.setCursor(0,0);
+  lcd.print("        ");
+  lcd.setCursor(0,0);
+  if (abs(differenceX) > 200)
+  {
+    if (differenceX > 0)
+    {      
+      lcd.print("Avant");
+      delay(250);
+    }
+    else
+    {
+      lcd.print("Arrie");
+      delay(250);
+    }
+  }
+  else
+  {
+    lcd.print("Arret");
+  }
+}*/
+
+void getMove()
+{
+
+  int xRaw = ReadAxis(X_AXIS_PIN);
+  int yRaw = ReadAxis(Y_AXIS_PIN);
+  int zRaw = ReadAxis(Z_AXIS_PIN);
+
+  // Convert raw values to 'milli-Gs"
+  long xScaled = map(xRaw, RawMinRest, RawMaxRest, -1000, 1000);
+  long yScaled = map(yRaw, RawMinRest, RawMaxRest, -1000, 1000);
+  long zScaled = map(zRaw, RawMinRest, RawMaxRest, -1000, 1000) - 1000;
+
+
+  lcd.setCursor(0,0);
+  if (abs(xScaled) > 300)
+  {
+    if (xScaled > 0)
+    {      
+      lcd.print("Avant  ");
+    }
+    else if (xScaled < 0)
+    {
+      lcd.print("Arriere");
+    }
+  }
+  else
+  {
+    xRestCpt += 1;     
+    if (xRestCpt > 500) 
+    {
+      lcd.print("       ");
+      xRestCpt = 0;
+    }
+  }
+
+  lcd.setCursor(9,0);
+  if (abs(yScaled) > 300)
+  {
+    if (yScaled > 0)
+    {      
+      lcd.print("Gauche");
+    }
+    else if (yScaled < 0)
+    {
+      lcd.print("Droite");
+    }
+  }
+  else
+  {
+    yRestCpt += 1;     
+    if (yRestCpt > 500) 
+    {
+      lcd.print("      ");
+      yRestCpt = 0;
+    }
+  }
+
+  lcd.setCursor(0,1);
+  if (abs(zScaled) > 400)
+  {
+    if (zScaled > 0)
+    {      
+      lcd.print("Bas ");
+    }
+    else if (zScaled < 0)
+    {
+      lcd.print("Haut");
+    }
+  }
+  else
+  {
+    zRestCpt += 1;     
+    if (zRestCpt > 500) 
+    {
+      lcd.print("    ");
+      zRestCpt = 0;
+    }
+  }
 }
